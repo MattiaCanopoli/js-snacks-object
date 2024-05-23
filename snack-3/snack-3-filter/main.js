4cfd5856827ffc3c8c58abbfb8f2b1d3c3a19e70
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-05-23 22:36:34 +0200

    fix: folder names. add: comments

--- snack-3/snack-3-filter/main.js
+++ snack-3/snack-3-filter/main.js
@@ -1,5 +1,5 @@
 'use strict'
-
+//definisco un array di oggetti 'animali'
 const animali = [
     {
         nome: 'animale1',
@@ -33,12 +33,30 @@ const animali = [
     }
 ]
 
-const mammiferi = []
+/*SOLUZIONE PIÙ LUNGA
+1.assegno alla variabile mammiferi il metodo filter dell'array animali
+2. verifico che la condizione sia vera, ovvero che la propietà classe dell'oggetto iterato sia 'mammiferi'
+3. se vero, ritorno l'oggetto iterato, che verrà aggiunto all'array mammiferi
+*/
 
-animali.forEach(function (animale) {
-    if (animale.classe === 'mammiferi') {
-        mammiferi.push(animale)
-    }
+
+// const mammiferi = animali.filter(function (animale) {
+//     if (animale.classe === 'mammiferi') {
+//         return animale
+//     }
+// })
+
+
+/*SOLUZIUONE BREVE
+1.assegno alla variabile mammiferi il metodo filter dell'array animali
+2.ritorno l'oggetto se vero (il codice potrebbe essere letto come return true,
+quindi se la classa di animale è diversa da 'mammiferi', è false e passa all'iterazione successiva)
+*/
+
+const mammiferi = animali.filter(function (animale) {
+
+    return animale.classe === 'mammiferi'
 })
 
+//stampo in console il nuovo array
 console.log(mammiferi)
--- snack-3/snack-3-forEach/main.js
+++ snack-3/snack-3-forEach/main.js
@@ -1,5 +1,6 @@
 'use strict'
 
+//definisco un array di oggetti 'animali'
 const animali = [
     {
         nome: 'animale1',
@@ -33,12 +34,21 @@ const animali = [
     }
 ]
 
-const mammiferi = animali.filter(function (animale) {
+//definisco una variabile 'mammiferi' che contine un array vuoto
+const mammiferi = []
+
+
+/*CICLO FOR EACH
+1.ad ogni iterazione, quindi per tutta la lunhezza dell'array, verifico che la proprietà classe dell'oggetto sia 'mammiferi'
+2.se la condizione è verificata, pusho l'oggetto nell'array 'mammiferi'
+*/
+animali.forEach(function (animale) {
     if (animale.classe === 'mammiferi') {
-        return animale
+        mammiferi.push(animale)
     }
 })
 
-
-
+//stampo in console l'array 'mammiferi'
 console.log(mammiferi)
+
+// NOTA: il metodo forEach, a differenza di map e filter, non ritorna nulla. È quindi necessario creare prima un array da riempire
