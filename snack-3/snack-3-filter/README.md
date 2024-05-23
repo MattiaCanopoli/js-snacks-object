9734f1cbe17f37182928cab9825711a822502863
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-05-23 21:20:38 +0200

    add: forEach and filter solution

---
+++ snack-3/snack-3-filter/README.md
@@ -0,0 +1,16 @@
+# Snack 3
+Crea un array di oggetti che rappresentano degli animali.
+Ogni animale ha un nome, una famiglia e una classe.
+Es:
+[
+  { nome: 'leone', famiglia: 'felidi', classe: 'mammiferi' },
+  { nome: 'cane', famiglia: 'canidi', classe: 'mammiferi' },
+  { nome: 'gallina', famiglia: 'fasianidi', classe: 'uccelli' },
+]
+Crea un nuovo array con la lista dei mammiferi
+
+
+1. creare array01 - animali - con oggetti animali
+2. creare array02 - mammiferi - vuoto
+3. iterare array01 con ciclo for
+4. per ogni oggetto, verificare la classe. se mammiferi, push in array02
---
+++ snack-3/snack-3-filter/main.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const animali = [
+    {
+        nome: 'animale1',
+        famiglia: 'famiglia1',
+        classe: 'mammiferi'
+    },
+    {
+        nome: 'animale2',
+        famiglia: 'famiglia2',
+        classe: 'ovipari'
+    },
+    {
+        nome: 'animale3',
+        famiglia: 'famiglia3',
+        classe: 'ovovivipari'
+    },
+    {
+        nome: 'animale4',
+        famiglia: 'famiglia1',
+        classe: 'mammiferi'
+    },
+    {
+        nome: 'animale5',
+        famiglia: 'famiglia2',
+        classe: 'mammiferi'
+    },
+    {
+        nome: 'animale6',
+        famiglia: 'famiglia3',
+        classe: 'ovovivipari'
+    }
+]
+
+const mammiferi = []
+
+animali.forEach(function (animale) {
+    if (animale.classe === 'mammiferi') {
+        mammiferi.push(animale)
+    }
+})
+
+console.log(mammiferi)
