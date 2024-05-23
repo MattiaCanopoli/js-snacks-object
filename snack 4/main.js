21c5f8a1d8cb7c1eb811e7511cd74b0d86abdfc5
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-05-23 22:03:56 +0200

    add: comments

--- snack 4/main.js
+++ snack 4/main.js
@@ -1,5 +1,5 @@
 'use strict'
-
+//creazione dell'array iniziale
 const persone01 = [
     {
         nome: 'mario',
@@ -22,9 +22,16 @@ const persone01 = [
         eta: 78
     },
 ]
-
+//creazione di un array vuoto per contenere i nuovi oggetti
 const persone02 = []
 
+/*
+CICLO FOR:
+1. crea variabile 'patente' con template literal nome + cognome + può guidare
+2. valuta l'attributo eta dell'oggetto. se minore di 18 la stringa in 'patente' diventa 'non puà guidare'
+3. agginge proprietà info = patente all'oggetto
+4. pusha l'oggetto nell'array persone02
+*/
 for (let i = 0; i < persone01.length; i++) {
     let patente = `${persone01[i].nome} ${persone01[i].cognome} può guidare`
 
@@ -35,7 +42,20 @@ for (let i = 0; i < persone01.length; i++) {
     persone01[i].info = patente
 
     persone02.push(persone01[i])
-
 }
 
+//stampa in console l'array persone02
 console.log(persone02)
+
+/*
+NOTA: Questo sistema non funzione perchè il ciclo aggiunge la proprietà info all'oggetto originale, 
+quindi stampando persona01 si ottiene lo stesso risultato che stampando persona02.
+Anche creando una nuova variabile newObj ed assegnandole persone01[i] il risultato sarebbe lo stesso, perchè il riferimento sarebbe sempre lo stesso oggetto.
+la soluzione ottimale spread creare un nuovo oggetto newObj ed utilizzare il metodo spread
+
+newObj = {
+    ...persone01[i]
+}
+
+a questo punto si può aggiungere info a newObj, quindi pusharlo nell'array persone02
+*/
--- snack 4/snack 4 map/main.js
+++ snack 4/snack 4 map/main.js
@@ -1,4 +1,5 @@
 'use strict'
+//creazione array di oggetti
 
 const persone01 = [
     {
@@ -23,6 +24,15 @@ const persone01 = [
     },
 ]
 
+/*
+1. utilizzo il metodo map degli array e lo assegno ad una variabile persone02. (gli argomenti index e array non sono utilizzati, ma scritti come memo)
+2. all'interno della funzione anonima di .map, definisco una variabile info contenente la stringa 'può guidare'
+3. verifico la proprietà eta dell'oggetto iterato. se < 18, il valore di info diventa 'non può guidare'
+4. creo un nuovo oggetto newObj nel quale copio le proprietà dell'oggetto iterato con il metodo spread e aggingo la proprietà info, che ha valore della variabile info
+5. ritorno il nuvo oggetto
+
+persone02 è un nuovo array con lunghezza pari all'array di origine (persone01), contenente tutti i newObj ai quali è stata aggiunta la proprietà info
+*/
 const persone02 = persone01.map(function (persona, index, array) {
     let info = 'può guidare'
 
@@ -39,6 +49,6 @@ const persone02 = persone01.map(function (persona, index, array) {
 }
 )
 
-
+//stampo entrambe gli array per verificare che siano effettivamente differenti
 console.log(persone02)
 console.log(persone01)
